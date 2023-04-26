9d6f7dc872a179ff944e43f1915811864ebdfc83
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-26 13:14:10 +0300

    login.js updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,21 +25,27 @@ function Login({ setIsLoggedIn }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
+    <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1 style={{ marginBottom: "20px" }}>Login</h1>
+      <label htmlFor="username">Username:</label>
       <input
         type="text"
         name="username"
+        id="username"
         value={formData.username}
         onChange={handleChange}
+        style={{ padding: "10px", marginBottom: "10px", width: "100%", maxWidth: "400px", borderRadius: "5px", border: "none", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.3)" }}
       />
+      <label htmlFor="password">Password:</label>
       <input
         type="password"
         name="password"
+        id="password"
         value={formData.password}
         onChange={handleChange}
+        style={{ padding: "10px", marginBottom: "20px", width: "100%", maxWidth: "400px", borderRadius: "5px", border: "none", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.3)" }}
       />
-      <button type="submit">Login</button>
+      <button type="submit" style={{ padding: "10px", width: "100%", maxWidth: "400px", backgroundColor: "#4CAF50", color: "white", border: "none", borderRadius: "5px", cursor: "pointer", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.3)" }}>Login</button>
     </form>
   );
 }
