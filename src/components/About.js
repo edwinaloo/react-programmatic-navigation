51aedbd015cc6b00f1c37911c72ad99e080a4b0b
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-26 12:54:12 +0300

    added pics and edit on about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function About() {
-  return <h1>This is my about component!</h1>;
+  return (
+    <div>
+      <h1>This is my about component. On my about component I have a few details about the home page</h1>
+      <img
+        src="https://example.com/image1.jpg"
+        alt="A beautiful image 1"
+        style={{ width: "50%", height: "auto", display: "block", margin: "auto" }}
+      />
+      <img
+        src="https://example.com/image2.jpg"
+        alt="A beautiful image 2"
+        style={{ width: "50%", height: "auto", display: "block", margin: "auto" }}
+      />
+      <img
+        src="https://example.com/image3.jpg"
+        alt="A beautiful image 3"
+        style={{ width: "50%", height: "auto", display: "block", margin: "auto" }}
+      />
+    </div>
+  );
 }
 
 export default About;
